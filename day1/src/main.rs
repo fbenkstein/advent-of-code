9b9ad4f81571b0e63d9ebfd96559109494ee47c8
Gabriel Feron <feron.gabriel@gmail.com>
2018-12-01 18:40:35 +0100

    Day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,7 @@
-use int_hash::IntHashMap;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process;
 
 fn main() {
     let file = File::open("input.txt").expect("file not found");
@@ -14,16 +15,17 @@ fn main() {
         .collect();
 
     let mut current_frequency: i32 = 0;
-    let mut frequencies: IntHashMap<i32, u32> = IntHashMap::default();
+    let mut frequencies: HashMap<i32, u32> = HashMap::default();
 
     loop {
         frequency_changes.iter().for_each(|frequency_change| {
             current_frequency += frequency_change;
             *frequencies.entry(current_frequency).or_insert(0) += 1;
             if *frequencies.entry(current_frequency).or_default() == 2 {
-                panic!(format!("Device calibrated! {}", current_frequency));
+                println!("Device calibrated: frequency is {} Hz", current_frequency);
+                process::exit(0);
             }
         });
-        println!("Calibrated frequency: {}", current_frequency);
+        print!("Current frequency: {} Hz\r", current_frequency);
     }
 }
