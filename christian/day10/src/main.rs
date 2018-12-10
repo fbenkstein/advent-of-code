f519326ad30c153cc8a64698ee1cfed020db30f3
Christian Vetter <veaac.fdirct@gmail.com>
2018-12-10 22:50:12 +0100

    Use cgmath

--- christian/day10/src/main.rs
+++ christian/day10/src/main.rs
@@ -1,35 +1,39 @@
 #[macro_use]
 extern crate text_io;
+use cgmath::{vec2, Vector2};
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::io::{self, prelude::*};
 
-#[derive(Debug, Default, Clone)]
-struct Point {
-    pos: (isize, isize),
-    speed: (isize, isize),
+#[derive(Debug, Clone)]
+struct Star {
+    pos: Vector2<isize>,
+    speed: Vector2<isize>,
 }
 
-fn parse(input: &Vec<String>) -> Vec<Point> {
+fn parse(input: &Vec<String>) -> Vec<Star> {
     let convert = |line: &String| {
-        let mut point: Point = Default::default();
-        scan!(line.bytes().filter(|&c| c != b' ') => "position=<{},{}>velocity=<{},{}>",point.pos.0,point.pos.1,point.speed.0,point.speed.1);
-        point
+        let (x, y, speed_x, speed_y): (isize, isize, isize, isize);
+        scan!(line.bytes().filter(|&c| c != b' ') => "position=<{},{}>velocity=<{},{}>", x, y, speed_x, speed_y);
+        Star {
+            pos: vec2(x, y),
+            speed: vec2(speed_x, speed_y),
+        }
     };
     input.iter().map(convert).collect()
 }
 
-fn print_solution(mut input: Vec<Point>, iterations: isize) -> Option<()> {
-    for x in input.iter_mut() {
-        x.pos.0 += x.speed.0 * iterations;
-        x.pos.1 += x.speed.1 * iterations;
+fn print_solution(mut input: Vec<Star>, iterations: isize) -> Option<()> {
+    for star in input.iter_mut() {
+        star.pos += star.speed * iterations;
     }
 
-    let dim_x = input.iter().map(|p| p.pos.0).minmax().into_option()?;
-    let dim_y = input.iter().map(|p| p.pos.1).minmax().into_option()?;
+    let dim_x = input.iter().map(|p| p.pos.x).minmax().into_option()?;
+    let dim_y = input.iter().map(|p| p.pos.y).minmax().into_option()?;
     let mut matrix = vec![' '; ((dim_x.1 - dim_x.0 + 1) * (dim_y.1 - dim_y.0 + 1)) as usize];
-    for x in input {
-        matrix[(x.pos.0 - dim_x.0 + (x.pos.1 - dim_y.0) * (dim_x.1 - dim_x.0 + 1)) as usize] = '*';
+    for star in input {
+        let pos = star.pos - Vector2::new(dim_x.0, dim_y.0);
+        matrix[(pos.x + pos.y * (dim_x.1 - dim_x.0 + 1)) as usize] = '*';
     }
 
     for (i, c) in matrix.iter().enumerate() {
@@ -41,20 +45,20 @@ fn print_solution(mut input: Vec<Point>, iterations: isize) -> Option<()> {
     Some(())
 }
 
-fn solve(input: &Vec<Point>) {
+fn solve(input: &Vec<Star>) {
     let mut freqs = HashMap::new();
     for p1 in input {
         for p2 in input {
             // check if the two points can be aligned on x axis
-            let diff = (p1.pos.0 - p2.pos.0, p1.pos.1 - p2.pos.1);
-            let diff_speed = (p2.speed.0 - p1.speed.0, p2.speed.1 - p1.speed.1);
-            if diff_speed.0 != 0
-                && diff_speed.0.signum() == diff.0.signum()
-                && diff.0 % diff_speed.0 == 0
+            let diff = p1.pos - p2.pos;
+            let diff_speed = p2.speed - p1.speed;
+            if diff_speed.x != 0
+                && diff_speed.x.signum() == diff.x.signum()
+                && diff.x % diff_speed.x == 0
             {
                 // check if they would end up next to each other on y axis
-                let iterations = diff.0 / diff_speed.0;
-                let distance = diff.1 - diff_speed.1 * iterations;
+                let iterations = diff.x / diff_speed.x;
+                let distance = diff.y - diff_speed.y * iterations;
                 if distance == 1 || distance == -1 {
                     *freqs.entry(iterations).or_insert(0) += 1;
                 }
