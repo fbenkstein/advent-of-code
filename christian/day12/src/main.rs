6260fce7f481a008a93d28b54459649f438c821d
Christian Vetter <veaac.fdirct@gmail.com>
2018-12-12 18:22:05 +0100

    Simplify

--- christian/day12/src/main.rs
+++ christian/day12/src/main.rs
@@ -1,8 +1,7 @@
 #[macro_use]
 extern crate text_io;
-
-use std::collections::HashMap;
 use std::io::{self, prelude::*};
+use std::ops::Range;
 
 struct Input {
     initial: Vec<bool>,
@@ -12,11 +11,9 @@ struct Input {
 fn parse(input: &Vec<String>) -> Input {
     let to_bool = |c| c == '#';
     let state: String;
-    let first_line = &input[0];
-    scan!(first_line.bytes() => "initial state: {}", state);
+    scan!((&input[0]).bytes() => "initial state: {}", state);
     let convert = |line: &String| {
-        let from: String;
-        let to: char;
+        let (from, to): (String, char);
         scan!(line.bytes() => "{} => {}", from, to);
         (from.chars().map(to_bool).collect(), to_bool(to))
     };
@@ -26,30 +23,32 @@ fn parse(input: &Vec<String>) -> Input {
     }
 }
 
-fn solve(input: &Input, max_iter: usize, num_iter: usize) {
+fn active_range(state: &Vec<bool>) -> Range<usize> {
+    Range {
+        start: state.iter().position(|x| *x).unwrap(),
+        end: state.len() - state.iter().rev().position(|x| *x).unwrap() + 1,
+    }
+}
+
+fn solve(Input { initial, rules }: &Input, max_iter: usize, num_iter: usize) {
     let mut state = vec![false; max_iter * 2];
-    state.extend(input.initial.iter());
+    state.extend(initial.iter());
     state.extend(vec![false; max_iter * 2]);
 
-    let mut lookup: HashMap<Vec<bool>, usize> = HashMap::new();
     let mut offset: isize = 0;
     for iter in 0..max_iter {
         let mut next_state = vec![false; state.len()];
-        for rule in input.rules.iter() {
-            for pos in 0..state.len() - 5 {
-                if rule.0[..] == state[pos..pos + 5] {
-                    next_state[pos + 2] = rule.1;
-                }
-            }
+        for pos in 0..state.len() - 5 {
+            let rule = rules.iter().find(|rule| rule.0[..] == state[pos..pos + 5]);
+            next_state[pos + 2] = rule.map(|rule| rule.1).unwrap_or(false);
         }
-        state = next_state;
-        let first = state.iter().position(|x| *x).unwrap();
-        let last = state.len() - state.iter().rev().position(|x| *x).unwrap();
-        if let Some(prev_first) = lookup.insert(state[first..=last].into(), first) {
-            println!("Stabilized {}, offset {} -> {}", iter, prev_first, first);
-            offset = (num_iter - 1 - iter) as isize * (first as isize - prev_first as isize);
+        let (prev, next) = (active_range(&state), active_range(&next_state));
+        if state[prev.clone()] == next_state[next.clone()] {
+            println!("Stabilized {}: {} -> {}", iter, prev.start, next.start);
+            offset = (num_iter - iter) as isize * (next.start as isize - prev.start as isize);
             break;
         }
+        state = next_state;
     }
 
     let eval = |(pos, x): (_, &_)| *x as isize * (pos as isize - (2 * max_iter as isize) + offset);
