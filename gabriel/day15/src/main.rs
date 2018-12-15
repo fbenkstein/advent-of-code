95dce8fc61a968cec5eb4dd8f2839571b03f9cf9
Gabriel Feron <feron.gabriel@gmail.com>
2018-12-15 13:42:35 +0100

    Parser for day 15

---
+++ gabriel/day15/src/main.rs
@@ -0,0 +1,16 @@
+mod parser;
+
+use crate::parser::Board;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::str::FromStr;
+
+fn main() {
+    let mut file = File::open("input.txt").expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("could not read file");
+    let board = Board::from_str(&contents).expect("could not parse board.");
+    println!("{}", board);
+}
---
+++ gabriel/day15/src/parser.rs
@@ -0,0 +1,115 @@
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Tile {
+    Wall,
+    Open,
+}
+
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Tile::Wall => write!(f, "#"),
+            Tile::Open => write!(f, "."),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Class {
+    Goblin,
+    Elf,
+}
+
+impl fmt::Display for Class {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Class::Goblin => write!(f, "G"),
+            Class::Elf => write!(f, "E"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Unit {
+    pub class: Class,
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug)]
+pub struct Board {
+    tiles: Vec<Tile>,
+    width: usize,
+    height: usize,
+    units: Vec<Unit>,
+}
+
+impl FromStr for Board {
+    type Err = String;
+    fn from_str(contents: &str) -> Result<Board, Self::Err> {
+        let height = contents.lines().count();
+        let width = contents
+            .lines()
+            .map(|line| line.len())
+            .max()
+            .expect("no max?!");
+        let mut tiles = Vec::with_capacity(width * height);
+        let mut units = vec![];
+
+        for (y, line) in contents.lines().enumerate() {
+            for (x, tile) in line.chars().enumerate() {
+                match tile {
+                    'E' => {
+                        units.push(Unit {
+                            class: Class::Elf,
+                            x: x,
+                            y: y,
+                        });
+                        tiles.push(Tile::Open);
+                    }
+                    'G' => {
+                        units.push(Unit {
+                            class: Class::Goblin,
+                            x: x,
+                            y: y,
+                        });
+                        tiles.push(Tile::Open);
+                    }
+                    '#' => tiles.push(Tile::Wall),
+                    '.' => {
+                        tiles.push(Tile::Open);
+                    }
+                    _ => return Err(format!("Invalid tile: {}", tile)),
+                }
+            }
+            for i in line.chars().count()..width {
+                tiles.insert(i, Tile::Wall);
+            }
+        }
+
+        Ok(Board {
+            tiles: tiles,
+            units: units,
+            height: height,
+            width: width,
+        })
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.height {
+            writeln!(f, "")?;
+            for x in 0..self.width {
+                if let Some(unit) = self.units.iter().find(|p| p.x == x && p.y == y) {
+                    write!(f, "{}", unit.class)?;
+                } else {
+                    write!(f, "{}", self.tiles[x + y * self.width])?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
