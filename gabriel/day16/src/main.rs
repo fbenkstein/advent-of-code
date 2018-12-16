74d9c37daf8887186d13f3eeb969eb675809fa9f
Gabriel Feron <feron.gabriel@gmail.com>
2018-12-17 00:47:27 +0100

    Switch day 16 to enum

--- gabriel/day16/src/main.rs
+++ gabriel/day16/src/main.rs
@@ -1,89 +1,122 @@
-pub struct Test {
-    before: [usize; 4],
-    after: [usize; 4],
-    opcode: usize,
-    args: (usize, usize, usize),
-}
-
-impl Test {
-    fn try_opcodes(&self) -> Vec<String> {
-        let before = self.before;
-        let after = self.after;
-        let args = self.args;
-        let mut possible_opcodes: Vec<String> = vec![];
-        Self::try_opcode(&mut possible_opcodes, "addr", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] + r[b]
-        });
-        Self::try_opcode(&mut possible_opcodes, "addi", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] + b
-        });
-        Self::try_opcode(&mut possible_opcodes, "mulr", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] * r[b]
-        });
-        Self::try_opcode(&mut possible_opcodes, "muli", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] * b
-        });
-        Self::try_opcode(&mut possible_opcodes, "banr", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] & r[b]
-        });
-        Self::try_opcode(&mut possible_opcodes, "bani", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] & b
-        });
-        Self::try_opcode(&mut possible_opcodes, "borr", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] | r[b]
-        });
-        Self::try_opcode(&mut possible_opcodes, "bori", before, after, args, &|r, (a, b, c)| {
-            r[c] = r[a] | b
-        });
-        Self::try_opcode(&mut possible_opcodes, "setr", before, after, args, &|r, (a, _, c)| {
-            r[c] = r[a]
-        });
-        Self::try_opcode(&mut possible_opcodes, "seti", before, after, args, &|r, (a, _, c)| {
-            r[c] = a
-        });
-        Self::try_opcode(&mut possible_opcodes, "gtir", before, after, args, &|r, (a, b, c)| {
-            r[c] = if a > r[b] { 1 } else { 0 }
-        });
-        Self::try_opcode(&mut possible_opcodes, "gtri", before, after, args, &|r, (a, b, c)| {
-            r[c] = if r[a] > b { 1 } else { 0 }
-        });
-        Self::try_opcode(&mut possible_opcodes, "gtrr", before, after, args, &|r, (a, b, c)| {
-            r[c] = if r[a] > r[b] { 1 } else { 0 }
-        });
-        Self::try_opcode(&mut possible_opcodes, "eqir", before, after, args, &|r, (a, b, c)| {
-            r[c] = if a == r[b] { 1 } else { 0 }
-        });
-        Self::try_opcode(&mut possible_opcodes, "eqri", before, after, args, &|r, (a, b, c)| {
-            r[c] = if r[a] == b { 1 } else { 0 }
-        });
-        Self::try_opcode(&mut possible_opcodes, "eqrr", before, after, args, &|r, (a, b, c)| {
-            r[c] = if r[a] == r[b] { 1 } else { 0 }
-        });
-
-        possible_opcodes
-    }
-
-    fn try_opcode(
-        possible_opcodes: &mut Vec<String>,
-        opcode: &str,
-        before: [usize; 4],
-        after: [usize; 4],
-        args: (usize, usize, usize),
-        operation: &Fn(&mut [usize; 4], (usize, usize, usize)),
-    ) {
-        let registers = &mut before.clone();
-        operation(registers, args);
-        if *registers == after {
-            possible_opcodes.push(opcode.into());
-        }
-    }
-}
-
 use std::fs::File;
 use std::io::prelude::*;
 
 use nom::types::CompleteStr;
 use nom::*;
+
+pub struct Test {
+    before: [usize; 4],
+    after: [usize; 4],
+    instruction: Instruction,
+}
+
+struct Instruction {
+    opcode: usize,
+    a: usize,
+    b: usize,
+    c: usize,
+}
+
+#[allow(non_camel_case_types)]
+pub enum Opcode {
+    addr(usize, usize, usize),
+    addi(usize, usize, usize),
+    mulr(usize, usize, usize),
+    muli(usize, usize, usize),
+    banr(usize, usize, usize),
+    bani(usize, usize, usize),
+    borr(usize, usize, usize),
+    bori(usize, usize, usize),
+    setr(usize, usize, usize),
+    seti(usize, usize, usize),
+    gtir(usize, usize, usize),
+    gtri(usize, usize, usize),
+    gtrr(usize, usize, usize),
+    eqir(usize, usize, usize),
+    eqri(usize, usize, usize),
+    eqrr(usize, usize, usize),
+}
+
+impl Opcode {
+    fn all(i: &Instruction) -> [Opcode; 16] {
+        [
+            Opcode::addr(i.a, i.b, i.c),
+            Opcode::addi(i.a, i.b, i.c),
+            Opcode::mulr(i.a, i.b, i.c),
+            Opcode::muli(i.a, i.b, i.c),
+            Opcode::banr(i.a, i.b, i.c),
+            Opcode::bani(i.a, i.b, i.c),
+            Opcode::borr(i.a, i.b, i.c),
+            Opcode::bori(i.a, i.b, i.c),
+            Opcode::setr(i.a, i.b, i.c),
+            Opcode::seti(i.a, i.b, i.c),
+            Opcode::gtir(i.a, i.b, i.c),
+            Opcode::gtri(i.a, i.b, i.c),
+            Opcode::gtrr(i.a, i.b, i.c),
+            Opcode::eqir(i.a, i.b, i.c),
+            Opcode::eqri(i.a, i.b, i.c),
+            Opcode::eqrr(i.a, i.b, i.c),
+        ]
+    }
+
+    fn from_instruction(i: &Instruction, vtable: &[usize; 16]) -> Self {
+        let operation_pos = vtable[i.opcode];
+        match operation_pos {
+            0 => Opcode::addr(i.a, i.b, i.c),
+            1 => Opcode::addi(i.a, i.b, i.c),
+            2 => Opcode::mulr(i.a, i.b, i.c),
+            3 => Opcode::muli(i.a, i.b, i.c),
+            4 => Opcode::banr(i.a, i.b, i.c),
+            5 => Opcode::bani(i.a, i.b, i.c),
+            6 => Opcode::borr(i.a, i.b, i.c),
+            7 => Opcode::bori(i.a, i.b, i.c),
+            8 => Opcode::setr(i.a, i.b, i.c),
+            9 => Opcode::seti(i.a, i.b, i.c),
+            10 => Opcode::gtir(i.a, i.b, i.c),
+            11 => Opcode::gtri(i.a, i.b, i.c),
+            12 => Opcode::gtrr(i.a, i.b, i.c),
+            13 => Opcode::eqir(i.a, i.b, i.c),
+            14 => Opcode::eqri(i.a, i.b, i.c),
+            15 => Opcode::eqrr(i.a, i.b, i.c),
+            _ => panic!("INVALID OPCODE"),
+        }
+    }
+
+    fn execute(&self, r: &mut [usize; 4]) {
+        match *self {
+            Opcode::addr(a, b, c) => r[c] = r[a] + r[b],
+            Opcode::addi(a, b, c) => r[c] = r[a] + b,
+            Opcode::mulr(a, b, c) => r[c] = r[a] * r[b],
+            Opcode::muli(a, b, c) => r[c] = r[a] * b,
+            Opcode::banr(a, b, c) => r[c] = r[a] & r[b],
+            Opcode::bani(a, b, c) => r[c] = r[a] & b,
+            Opcode::borr(a, b, c) => r[c] = r[a] | r[b],
+            Opcode::bori(a, b, c) => r[c] = r[a] | b,
+            Opcode::setr(a, _, c) => r[c] = r[a],
+            Opcode::seti(a, _, c) => r[c] = a,
+            Opcode::gtir(a, b, c) => r[c] = if a > r[b] { 1 } else { 0 },
+            Opcode::gtri(a, b, c) => r[c] = if r[a] > b { 1 } else { 0 },
+            Opcode::gtrr(a, b, c) => r[c] = if r[a] > r[b] { 1 } else { 0 },
+            Opcode::eqir(a, b, c) => r[c] = if a == r[b] { 1 } else { 0 },
+            Opcode::eqri(a, b, c) => r[c] = if r[a] == b { 1 } else { 0 },
+            Opcode::eqrr(a, b, c) => r[c] = if r[a] == r[b] { 1 } else { 0 },
+        }
+    }
+}
+
+impl Test {
+    fn count_opcodes_that_work(&self) -> usize {
+        let after = self.after;
+        Opcode::all(&self.instruction)
+            .into_iter()
+            .filter(|o| {
+                let mut registers = self.before.clone();
+                o.execute(&mut registers);
+                registers == after
+            })
+            .count()
+    }
+}
 
 named!(parse_usize(CompleteStr) -> usize,
     map_res!(
@@ -108,7 +141,9 @@ named!(pub parse_test<CompleteStr, Test>,
         line_ending >>
         opcode: parse_usize >>
         space0 >>
-        args: tuple!(parse_usize, parse_usize, parse_usize) >>
+        a: parse_usize >>
+        b: parse_usize >>
+        c: parse_usize >>
         line_ending >>
         tag!("After:") >>
         space0 >>
@@ -116,7 +151,7 @@ named!(pub parse_test<CompleteStr, Test>,
         after: count_fixed!(usize, parse_usize, 4) >>
         tag!("]") >>
         opt!(line_ending) >>
-        (Test { before, after, opcode, args })
+        (Test { before, after, instruction: Instruction { opcode, a, b, c } })
     )
 );
 
@@ -135,6 +170,20 @@ fn main() {
 
     println!(
         "Number of samples that behave like three or more opcodes: {}",
-        tests.iter().filter(|t| t.try_opcodes().len() >= 3).count()
+        tests.iter().filter(|&t| t.count_opcodes_that_work() >= 3).count()
     );
+
+    // Part two
+    // let mut opcodes: Vec<HashMap<String, usize>> = vec![];
+    // for _ in 0..16 {
+    //     opcodes.push(HashMap::new());
+    // }
+    // for test in tests.iter() {
+    //     let compatible_opcodes = test.try_opcodes();
+    //     for opcode in compatible_opcodes {
+    //         *opcodes[test.opcode].entry(opcode).or_default() += 1;
+    //     }
+    // }
+
+    // println!("{:?}", opcodes);
 }
