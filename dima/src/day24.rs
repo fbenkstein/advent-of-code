700484b836a2fccb45b2d3146a946d08b63c486e
boxdot <d@zerovolt.org>
2019-01-04 20:30:15 +0100

    Dima's day 24 solution part 2.

--- dima/src/day24.rs
+++ dima/src/day24.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::collections::HashSet;
 use text_io::{scan, try_scan};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Army {
     ImmuneSystem,
     Infection,
@@ -14,24 +14,24 @@ impl Default for Army {
     }
 }
 
-#[derive(Default, Debug, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
 struct Group {
     army: Army,
-    units: u32,
-    hit_points: u32,
+    units: u64,
+    hit_points: u64,
     weaknesses: HashSet<String>,
     immunities: HashSet<String>,
-    attack_damage: u32,
+    attack_damage: u64,
     attack_type: String,
-    initiative: u32,
+    initiative: u64,
 }
 
 impl Group {
-    fn effective_power(&self) -> u32 {
+    fn effective_power(&self) -> u64 {
         self.units * self.attack_damage
     }
 
-    fn damage_to(&self, other: &Self) -> u32 {
+    fn damage_to(&self, other: &Self) -> u64 {
         assert!(self.army != other.army);
         if other.immunities.contains(&self.attack_type) {
             0
@@ -50,7 +50,7 @@ impl Ord for Group {
         self.effective_power()
             .cmp(&other.effective_power())
             .reverse()
-            .then(self.initiative.cmp(&other.initiative))
+            .then(self.initiative.cmp(&other.initiative).reverse())
     }
 }
 
@@ -166,35 +166,66 @@ fn target_selection(groups: &mut [Group]) -> Vec<Option<usize>> {
         .collect()
 }
 
-fn attacking(groups: &mut Vec<Group>, targets: &[Option<usize>]) {
+fn attacking(groups: &mut Vec<Group>, targets: &[Option<usize>]) -> bool {
     let mut order: Vec<usize> = (0..groups.len()).collect();
     order.sort_by_key(|idx| {
         let g = &groups[*idx];
         -(g.initiative as i32)
     });
+    let mut anyone_killed = false;
     for idx in order {
         if let Some(target_idx) = targets[idx] {
-            let attacking = &groups[idx];
             let defending = &groups[target_idx];
+            let attacking = &groups[idx];
             let damage = attacking.damage_to(defending);
-            let killed = damage / defending.hit_points;
+            let killed = defending.units.min(damage / defending.hit_points);
+            if killed > 0 {
+                anyone_killed = true;
+            }
             let new_units = defending.units.saturating_sub(killed);
             groups[target_idx].units = new_units;
         }
     }
 
     groups.retain(|g| g.units > 0);
+    anyone_killed
 }
 
-pub fn solve(input: &str) -> u32 {
-    let mut groups = parse(input);
+fn run_with_boost(mut groups: Vec<Group>, boost: u64) -> Option<(Army, u64)> {
+    for g in &mut groups {
+        if g.army == Army::ImmuneSystem {
+            g.attack_damage += boost;
+        }
+    }
+
     while groups.iter().any(|g| g.army == Army::ImmuneSystem)
         && groups.iter().any(|g| g.army == Army::Infection)
     {
         let targets = target_selection(&mut groups);
-        attacking(&mut groups, &targets[..]);
+        let anyone_killed = attacking(&mut groups, &targets[..]);
+        if !anyone_killed {
+            // we stuck in a loop
+            return None;
+        }
     }
-    groups.iter().map(|g| g.units).sum()
+
+    Some((groups[0].army, groups.iter().map(|g| g.units).sum()))
+}
+
+pub fn solve(input: &str) -> (u64, u64) {
+    let groups = parse(input);
+    let p1 = run_with_boost(groups.clone(), 0);
+
+    let mut p2 = 0;
+    for boost in 1.. {
+        if let Some((winning_army, units)) = run_with_boost(groups.clone(), boost) {
+            if winning_army == Army::ImmuneSystem {
+                p2 = units;
+                break;
+            }
+        }
+    }
+    (p1.unwrap().1, p2)
 }
 
 #[cfg(test)]
@@ -202,7 +233,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_target_selection() {
+    fn test_run_with_boost() {
         const INPUT: &str = r#"Immune System:
 17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
@@ -210,23 +241,11 @@ mod tests {
 Infection:
 801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
 4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4"#;
-        let mut groups = parse(INPUT);
-        println!("{:?}", groups);
-        let targets = target_selection(&mut groups);
-        println!("{:?}", targets);
-        for (idx, g) in groups.iter().enumerate() {
-            println!(
-                "{:?} -> {:?}",
-                g.units,
-                targets[idx].map(|i| groups[i].units)
-            );
-        }
-        let p1 = solve(INPUT);
-        assert_eq!(p1, 5216);
+        let groups = parse(INPUT);
+        assert_eq!(
+            run_with_boost(groups.clone(), 0),
+            Some((Army::Infection, 5216))
+        );
+        assert_eq!(run_with_boost(groups, 1570), Some((Army::ImmuneSystem, 51)));
     }
-
-    // 4485 -> 989
-    // 989 -> 801
-    // 17 -> 4485
-    // 801 -> 17
 }
